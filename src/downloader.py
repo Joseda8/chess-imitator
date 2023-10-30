c362a13ff33e8de6121ed37676d153f7e915932b
José Daniel Montoya Salazar <jd.montoya.s8@gmail.com>
2023-10-30 22:37:06 +0100

    Format changes in downloader.py

--- src/downloader.py
+++ src/downloader.py
@@ -89,8 +89,7 @@ class ChessDownloader:
 
 
 # Argument parser
-parser = argparse.ArgumentParser(
-    description='Download chess games and store them as PGN files.')
+parser = argparse.ArgumentParser(description='Download chess games and store them as PGN files.')
 parser.add_argument('--username', type=str, default='Joseda8', help='Username of the chess player.')
 parser.add_argument('--year', type=int, default=2023, help='Year of the games to download.')
 parser.add_argument('--remove', action='store_true', default=True, help='Remove existing files before downloading.')
